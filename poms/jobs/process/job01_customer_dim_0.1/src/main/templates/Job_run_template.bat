d5a21fcd88c23799855f3ad29147d302f9b586d2
Mohamed <ahnbi32@gmail.com>
2023-12-06 12:53:46 +0100

    First commit

---
+++ poms/jobs/process/job01_customer_dim_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.13.2.jar;../lib/log4j-api-2.13.2.jar;../lib/log4j-core-2.13.2.jar;../lib/log4j-1.2-api-2.13.2.jar;../lib/commons-collections-3.2.2.jar;../lib/jboss-marshalling-river-2.0.12.Final.jar;../lib/mysql-connector-java-5.1.49.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/advancedPersistentLookupLib-1.3.jar;../lib/jxl.jar;../lib/dom4j-2.1.3.jar;../lib/slf4j-api-1.7.29.jar;../lib/trove.jar;../lib/crypto-utils-0.31.12.jar;job01_customer_dim_0_1.jar; talend_project.job01_customer_dim_0_1.Job01_Customer_Dim %*
